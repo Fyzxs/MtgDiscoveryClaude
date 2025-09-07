3a058acd0050d2d3e35bf72a4989b783422e52fd
Quinn Gil <Fyzxs@users.noreply.github.com>
2025-09-07 14:01:26 -0700

    manual walk through2

---
+++ src/Lib.MtgDiscovery.Entry/Entities/UserInfoItrEntity.cs
@@ -0,0 +1,10 @@
+﻿using Lib.Shared.DataModels.Entities;
+
+namespace Lib.MtgDiscovery.Entry.Entities;
+
+internal sealed class UserInfoItrEntity : IUserInfoItrEntity
+{
+    public string UserId { get; init; }
+    public string UserSourceId { get; init; }
+    public string UserNickname { get; init; }
+}
