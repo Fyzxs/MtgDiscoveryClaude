7f49c0801e307d1022450cefadd74315655b07c3
Quinn Gil <Fyzxs@users.noreply.github.com>
2025-09-06 23:33:45 -0700

    minus blob

--- src/Lib.Adapter.Scryfall.BlobStorage/Values/SetIconBlobFriendlyContainerName.cs
+++
@@ -1,8 +0,0 @@
-﻿using Lib.BlobStorage.Apis.Configurations;
-
-namespace Lib.Adapter.Scryfall.BlobStorage.Values;
-
-internal sealed class SetIconBlobFriendlyContainerName : BlobFriendlyContainerName
-{
-    public override string AsSystemType() => "MtgSets";
-}
