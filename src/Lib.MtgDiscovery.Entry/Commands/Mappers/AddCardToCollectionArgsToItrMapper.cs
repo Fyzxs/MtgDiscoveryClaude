ef57d6ed4a2b7c3bef6e5737392e64d8b2be166a
Fyzxs <you@users.noreply.github.com>
2025-09-15 19:52:53 -0700

    more mapper fixes

--- src/Lib.MtgDiscovery.Entry/Commands/Mappers/AddCardToCollectionArgsToItrMapper.cs
+++ src/Lib.MtgDiscovery.Entry/Commands/Mappers/AddCardToCollectionArgsToItrMapper.cs
@@ -1,12 +1,9 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
 using Lib.MtgDiscovery.Entry.Commands.Entities;
-using Lib.Shared.Abstractions.Mappers;
 using Lib.Shared.DataModels.Entities;
 
 namespace Lib.MtgDiscovery.Entry.Commands.Mappers;
-
-internal interface IAddCardToCollectionArgsToItrMapper : ICreateMapper<IAddCardToCollectionArgEntity, IUserCardCollectionItrEntity>;
 
 internal sealed class AddCardToCollectionArgsToItrMapper : IAddCardToCollectionArgsToItrMapper
 {
---
+++ src/Lib.MtgDiscovery.Entry/Commands/Mappers/IAddCardToCollectionArgsToItrMapper.cs
@@ -0,0 +1,7 @@
+﻿using Lib.MtgDiscovery.Entry.Commands.Entities;
+using Lib.Shared.Abstractions.Mappers;
+using Lib.Shared.DataModels.Entities;
+
+namespace Lib.MtgDiscovery.Entry.Commands.Mappers;
+
+internal interface IAddCardToCollectionArgsToItrMapper : ICreateMapper<IAddCardToCollectionArgEntity, IUserCardCollectionItrEntity>;
