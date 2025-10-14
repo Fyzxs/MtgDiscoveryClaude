12642fefe29fba08d011ea1afc816dd2ea4159ec
Fyzxs <you@users.noreply.github.com>
2025-10-13 21:18:45 -0700

    feat: Add centralized icon imports from Material-UI Icons

---
+++ client/src/components/atoms/Icons.tsx
@@ -0,0 +1,40 @@
+/**
+ * Centralized icon imports from Material-UI Icons
+ * This ensures minimal imports and flexibility in icon sources
+ *
+ * Usage:
+ * import { CloseIcon, SearchIcon } from '../atoms/Icons';
+ */
+
+// Navigation Icons
+export { default as ArrowDropDownIcon } from '@mui/icons-material/ArrowDropDown';
+export { default as ChevronLeftIcon } from '@mui/icons-material/ChevronLeft';
+export { default as ExpandLessIcon } from '@mui/icons-material/ExpandLess';
+export { default as ExpandMoreIcon } from '@mui/icons-material/ExpandMore';
+export { default as KeyboardArrowUpIcon } from '@mui/icons-material/KeyboardArrowUp';
+export { default as NavigateBeforeIcon } from '@mui/icons-material/NavigateBefore';
+export { default as NavigateNextIcon } from '@mui/icons-material/NavigateNext';
+
+// Action Icons
+export { default as ClearIcon } from '@mui/icons-material/Clear';
+export { default as CloseIcon } from '@mui/icons-material/Close';
+export { default as ContentCopyIcon } from '@mui/icons-material/ContentCopy';
+export { default as FlipIcon } from '@mui/icons-material/Flip';
+export { default as OpenInNewIcon } from '@mui/icons-material/OpenInNew';
+export { default as RefreshIcon } from '@mui/icons-material/Refresh';
+export { default as SearchIcon } from '@mui/icons-material/Search';
+export { default as ZoomInIcon } from '@mui/icons-material/ZoomIn';
+
+// Status Icons
+export { default as CircleIcon } from '@mui/icons-material/Circle';
+export { default as CircleOutlinedIcon } from '@mui/icons-material/CircleOutlined';
+export { default as ErrorOutlineIcon } from '@mui/icons-material/ErrorOutline';
+export { default as HelpOutlineIcon } from '@mui/icons-material/HelpOutline';
+export { default as RemoveCircleIcon } from '@mui/icons-material/RemoveCircle';
+export { default as WarningIcon } from '@mui/icons-material/Warning';
+
+// Content Icons
+export { default as CollectionsBookmarkIcon } from '@mui/icons-material/CollectionsBookmark';
+export { default as ImageNotSupportedIcon } from '@mui/icons-material/ImageNotSupported';
+export { default as KeyboardIcon } from '@mui/icons-material/Keyboard';
+export { default as SearchOffIcon } from '@mui/icons-material/SearchOff';
