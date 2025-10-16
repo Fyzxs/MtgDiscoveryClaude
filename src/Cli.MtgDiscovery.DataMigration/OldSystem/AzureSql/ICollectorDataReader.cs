40ce563bdfe2d35a8498c22630ba2e34b9d59b49
Fyzxs <you@users.noreply.github.com>
2025-10-15 20:44:33 -0700

    T4.1

---
+++ src/Cli.MtgDiscovery.DataMigration/OldSystem/AzureSql/ICollectorDataReader.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Cli.MtgDiscovery.DataMigration.OldSystem.AzureSql.Entities;
+
+namespace Cli.MtgDiscovery.DataMigration.OldSystem.AzureSql;
+
+public interface ICollectorDataReader
+{
+    Task<IEnumerable<CollectorDataRecord>> ReadAllAsync(string collectorId);
+    Task<int> GetTotalCountAsync(string collectorId);
+}
