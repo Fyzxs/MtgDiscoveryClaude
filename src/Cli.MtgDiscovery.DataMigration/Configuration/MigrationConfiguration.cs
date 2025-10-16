5e2ac628a04621238f4aeaa831334f43ae900d2e
Fyzxs <you@users.noreply.github.com>
2025-10-15 20:33:31 -0700

    T2.1

---
+++ src/Cli.MtgDiscovery.DataMigration/Configuration/MigrationConfiguration.cs
@@ -0,0 +1,9 @@
+namespace Cli.MtgDiscovery.DataMigration.Configuration;
+
+public sealed class MigrationConfiguration
+{
+    public required string SourceCollectorId { get; init; }
+    public required string TargetUserId { get; init; }
+    public required string ErrorOutputPath { get; init; }
+    public required string SuccessOutputPath { get; init; }
+}
