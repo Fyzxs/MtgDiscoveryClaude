7a62d22beb1c2d56af615e1fa310f4c5be33c934
Fyzxs <you@users.noreply.github.com>
2025-10-15 21:41:46 -0700

    T15.2

--- src/Cli.MtgDiscovery.DataMigration/Program.cs
+++ src/Cli.MtgDiscovery.DataMigration/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Cli.MtgDiscovery.DataMigration;
+
+internal sealed class Program
+{
+    private static async Task<int> Main(string[] args)
+    {
+        IConfiguration configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: false)
+            .Build();
+
+        using ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder.AddConsole();
+            builder.SetMinimumLevel(LogLevel.Information);
+        });
+
+        DataMigrationApplication application = new DataMigrationApplication(configuration, loggerFactory);
+
+        try
+        {
+            int result = await application.RunAsync().ConfigureAwait(false);
+            return result;
+        }
+        catch (Exception ex)
+        {
+            ILogger logger = loggerFactory.CreateLogger<Program>();
+            logger.LogError(ex, "Unhandled exception in migration application");
+            return 1;
+        }
+    }
+}
