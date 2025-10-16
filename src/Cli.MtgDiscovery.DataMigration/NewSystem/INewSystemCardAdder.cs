4ca302fd7169d873ebb31300d1aca83dafbf7039
Fyzxs <you@users.noreply.github.com>
2025-10-15 21:10:50 -0700

    T9.1

---
+++ src/Cli.MtgDiscovery.DataMigration/NewSystem/INewSystemCardAdder.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Lib.MtgDiscovery.Entry.Entities;
+using Lib.MtgDiscovery.Entry.Entities.Outs.Cards;
+using Lib.Shared.Invocation.Operations;
+
+namespace Cli.MtgDiscovery.DataMigration.NewSystem;
+
+public interface INewSystemCardAdder
+{
+    Task<IOperationResponse<List<CardItemOutEntity>>> AddCardToCollectionAsync(IAddCardToCollectionArgsEntity args);
+}
