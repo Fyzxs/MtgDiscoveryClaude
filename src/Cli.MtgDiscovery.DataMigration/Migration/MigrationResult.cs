6a69f20b4da8d1b9e525b1215463e330224e81c0
Fyzxs <you@users.noreply.github.com>
2025-10-15 20:43:20 -0700

    TT3.5

---
+++ src/Cli.MtgDiscovery.DataMigration/Migration/MigrationResult.cs
@@ -0,0 +1,9 @@
+namespace Cli.MtgDiscovery.DataMigration.Migration;
+
+public sealed class MigrationResult
+{
+    public required int TotalRecords { get; init; }
+    public required int SuccessfulMigrations { get; init; }
+    public required int CardsNotFound { get; init; }
+    public required int OtherErrors { get; init; }
+}
