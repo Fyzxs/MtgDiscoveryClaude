c5907ba8eec4679a1878ebefac3fd125f645911b
Fyzxs <you@users.noreply.github.com>
2025-10-27 22:49:28 -0700

    agg sets to execute

--- src/Lib.Aggregator.Sets/Queries/SetsByCode/ISetsByCodeAggregatorService.cs
+++ src/Lib.Aggregator.Sets/Queries/SetsByCode/ISetsByCodeAggregatorService.cs
@@ -1,10 +1,7 @@
-﻿using System.Threading.Tasks;
-using Lib.Shared.DataModels.Entities.Itrs;
-using Lib.Shared.Invocation.Operations;
+﻿using Lib.Shared.DataModels.Entities.Itrs;
+using Lib.Shared.Invocation.Services;
 
 namespace Lib.Aggregator.Sets.Queries.SetsByCode;
 
 internal interface ISetsByCodeAggregatorService
-{
-    Task<IOperationResponse<ISetItemCollectionOufEntity>> SetsByCodeAsync(ISetCodesItrEntity args);
-}
+    : IOperationResponseService<ISetCodesItrEntity, ISetItemCollectionOufEntity>;
