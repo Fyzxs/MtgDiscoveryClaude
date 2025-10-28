aa48da008a55d5d14153f456c8c4e7c1dd467d23
Fyzxs <you@users.noreply.github.com>
2025-10-27 22:33:57 -0700

    Implement user registration services and interfaces

---
+++ src/Lib.Domain.User/Apis/IUserCommandDomainService.cs
@@ -0,0 +1,10 @@
+using System.Threading.Tasks;
+using Lib.Shared.DataModels.Entities.Itrs;
+using Lib.Shared.Invocation.Operations;
+
+namespace Lib.Domain.User.Apis;
+
+public interface IUserCommandDomainService
+{
+    Task<IOperationResponse<IUserInfoOufEntity>> RegisterUserAsync(IUserInfoItrEntity userInfo);
+}
