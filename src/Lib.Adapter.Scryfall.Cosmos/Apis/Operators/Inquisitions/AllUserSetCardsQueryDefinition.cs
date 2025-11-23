38a4eb5dcfac12e348d544b6285c2908889e2584
Fyzxs <fyzxs@users.noreply.github.com>
2025-11-22 23:28:54 -0800

    feat: Add new indexing policies for various entities and update query definition for user set cards

--- src/Lib.Adapter.Scryfall.Cosmos/Apis/Operators/Inquisitions/AllUserSetCardsQueryDefinition.cs
+++ src/Lib.Adapter.Scryfall.Cosmos/Apis/Operators/Inquisitions/AllUserSetCardsQueryDefinition.cs
@@ -1,4 +1,4 @@
-using Lib.Cosmos.Apis.Operators;
+﻿using Lib.Cosmos.Apis.Operators;
 using Microsoft.Azure.Cosmos;
 
 namespace Lib.Adapter.Scryfall.Cosmos.Apis.Operators.Inquisitions;
@@ -6,5 +6,5 @@ namespace Lib.Adapter.Scryfall.Cosmos.Apis.Operators.Inquisitions;
 internal sealed class AllUserSetCardsQueryDefinition : InquiryDefinition
 {
     public override QueryDefinition AsSystemType() =>
-        new("SELECT * FROM c WHERE c.user_id = @userId");
+        new("SELECT * FROM c WHERE c.partition = @userId");
 }
