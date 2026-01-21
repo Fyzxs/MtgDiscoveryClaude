ca2b72cfb0df5bb2dc7d2a99ea63aaed41eabda4
Fyzxs <fyzxs@users.noreply.github.com>
2026-01-20 20:28:32 -0800

    T11.1

---
+++ src/Cli.MtgDiscovery.DataMigration/Mapping/IOldToNewCardMapper.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using Cli.MtgDiscovery.DataMigration.OldSystem.AzureSql.Entities;
+using Cli.MtgDiscovery.DataMigration.OldSystem.Cosmos.Entities;
+using Lib.MtgDiscovery.Entry.Entities;
+using Lib.Shared.Abstractions.Mappers;
+using Lib.Shared.DataModels.Entities.Itrs;
+
+namespace Cli.MtgDiscovery.DataMigration.Mapping;
+
+public interface IOldToNewCardMapper : ICreateMapper<(CollectorDataRecord sqlRecord, OldDiscoveryCardExtEntity oldCosmosCard, ICardItemItrEntity newSystemCard, string targetUserId), IEnumerable<IAddCardToCollectionArgsEntity>>
+{
+}
