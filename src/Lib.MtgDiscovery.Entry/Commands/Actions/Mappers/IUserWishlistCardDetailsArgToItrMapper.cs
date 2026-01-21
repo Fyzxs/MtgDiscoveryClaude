cbded88bac416be510dbc702d8c9e6a63125c366
Fyzxs <fyzxs@users.noreply.github.com>
2026-01-20 20:31:21 -0800

    git silly

--- src/Lib.MtgDiscovery.Entry/Commands/Actions/Mappers/IUserWishlistCardDetailsArgToItrMapper.cs
+++ src/Lib.MtgDiscovery.Entry/Commands/Actions/Mappers/IUserWishlistCardDetailsArgToItrMapper.cs
@@ -1,10 +1,7 @@
-using System.Threading.Tasks;
+using Lib.Shared.Abstractions.Actions.Mappers;
 using Lib.Shared.DataModels.Entities.Args.UserWishlistCards;
 using Lib.Shared.DataModels.Entities.Itrs.UserWishlistCards;
 
 namespace Lib.MtgDiscovery.Entry.Commands.Actions.Mappers;
 
-internal interface IUserWishlistCardDetailsArgToItrMapper //TODO: Implement the interface all other mappers implement
-{
-    Task<IUserWishlistCardDetailsItrEntity> Map(IUserWishlistCardDetailsArgEntity argItem);
-}
+internal interface IUserWishlistCardDetailsArgToItrMapper : ICreateMapper<IUserWishlistCardDetailsArgEntity, IUserWishlistCardDetailsItrEntity>;
