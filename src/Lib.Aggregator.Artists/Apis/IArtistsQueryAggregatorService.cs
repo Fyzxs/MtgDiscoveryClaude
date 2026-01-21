8225ab743dd4c26e4696f726411f7ed5471ba808
Fyzxs <fyzxs@users.noreply.github.com>
2026-01-20 20:28:36 -0800

    agg artists to execute

---
+++ src/Lib.Aggregator.Artists/Apis/IArtistsQueryAggregatorService.cs
@@ -0,0 +1,12 @@
+using System.Threading.Tasks;
+using Lib.Shared.DataModels.Entities.Itrs;
+using Lib.Shared.Invocation.Operations;
+
+namespace Lib.Aggregator.Artists.Apis;
+
+public interface IArtistsQueryAggregatorService
+{
+    Task<IOperationResponse<IArtistSearchResultCollectionOufEntity>> ArtistSearchAsync(IArtistSearchTermItrEntity searchTerm);
+    Task<IOperationResponse<ICardItemCollectionOufEntity>> CardsByArtistAsync(IArtistIdItrEntity artistId);
+    Task<IOperationResponse<ICardItemCollectionOufEntity>> CardsByArtistNameAsync(IArtistNameItrEntity artistName);
+}
