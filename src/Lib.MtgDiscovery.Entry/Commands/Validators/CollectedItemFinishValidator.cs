18a178ca11021af31345904ce80447f5c9b89494
Fyzxs <fyzxs@users.noreply.github.com>
2026-01-20 20:26:31 -0800

    fix validators

--- src/Lib.MtgDiscovery.Entry/Commands/Validators/CollectedItemFinishValidator.cs
+++ src/Lib.MtgDiscovery.Entry/Commands/Validators/CollectedItemFinishValidator.cs
@@ -8,28 +8,29 @@ namespace Lib.MtgDiscovery.Entry.Commands.Validators;
 
 internal sealed class CollectedItemFinishValidator : OperationResponseValidator<IAddCardToCollectionArgEntity, IUserCardCollectionItrEntity>
 {
+    private static readonly HashSet<string> s_validFinishes = new(System.StringComparer.OrdinalIgnoreCase)
+    {
+        "nonfoil",
+        "foil",
+        "etched"
+    };
+
     public CollectedItemFinishValidator() : base(new Validator(), new Message())
     { }
 
     public sealed class Validator : IValidator<IAddCardToCollectionArgEntity>
     {
-        internal static readonly HashSet<string> ValidFinishes = new(System.StringComparer.OrdinalIgnoreCase)
-        {
-            "nonfoil",
-            "foil",
-            "etched"
-        };
 
         public Task<bool> IsValid(IAddCardToCollectionArgEntity arg)
         {
             if (arg.CollectedItem is null) return Task.FromResult(true); // Skip if null, handled by null validator
 
-            return Task.FromResult(ValidFinishes.Contains(arg.CollectedItem.Finish));
+            return Task.FromResult(s_validFinishes.Contains(arg.CollectedItem.Finish));
         }
     }
 
     public sealed class Message : OperationResponseMessage
     {
-        public override string AsSystemType() => $"Finish must be one of: {string.Join(", ", Validator.ValidFinishes)}";
+        public override string AsSystemType() => $"Finish must be one of: {string.Join(", ", s_validFinishes)}";
     }
 }
