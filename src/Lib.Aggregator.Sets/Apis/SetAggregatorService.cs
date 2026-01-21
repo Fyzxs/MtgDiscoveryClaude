13cc7d2f2896092a5b3cc35494f4107ced62b698
Fyzxs <fyzxs@users.noreply.github.com>
2026-01-20 20:28:37 -0800

    agg sets to execute

--- src/Lib.Aggregator.Sets/Apis/SetAggregatorService.cs
+++ src/Lib.Aggregator.Sets/Apis/SetAggregatorService.cs
@@ -8,16 +8,16 @@ namespace Lib.Aggregator.Sets.Apis;
 
 public sealed class SetAggregatorService : ISetAggregatorService
 {
-    private readonly ISetAggregatorService _querySetAggregatorService;
+    private readonly ISetsQueryAggregatorService _querySetAggregatorService;
 
     public SetAggregatorService(ILogger logger) : this(new SetsQueryAggregator(logger))
     { }
 
-    private SetAggregatorService(ISetAggregatorService querySetAggregatorService) => _querySetAggregatorService = querySetAggregatorService;
+    private SetAggregatorService(ISetsQueryAggregatorService querySetAggregatorService) => _querySetAggregatorService = querySetAggregatorService;
 
     public async Task<IOperationResponse<ISetItemCollectionOufEntity>> SetsAsync(ISetIdsItrEntity setIds) => await _querySetAggregatorService.SetsAsync(setIds).ConfigureAwait(false);
 
     public async Task<IOperationResponse<ISetItemCollectionOufEntity>> SetsByCodeAsync(ISetCodesItrEntity setCodes) => await _querySetAggregatorService.SetsByCodeAsync(setCodes).ConfigureAwait(false);
 
-    public async Task<IOperationResponse<ISetItemCollectionOufEntity>> AllSetsAsync() => await _querySetAggregatorService.AllSetsAsync().ConfigureAwait(false);
+    public async Task<IOperationResponse<ISetItemCollectionOufEntity>> AllSetsAsync(INoArgsItrEntity noArgs) => await _querySetAggregatorService.AllSetsAsync(noArgs).ConfigureAwait(false);
 }
