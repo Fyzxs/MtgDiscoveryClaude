6137f2d1530514347b951681f2f6f1e30a553622
Fyzxs <fyzxs@users.noreply.github.com>
2026-01-20 20:28:29 -0800

    fix: update CardCollectionUpdate interface to require setId and setGroupId fields

--- client/src/types/collection.ts
+++ client/src/types/collection.ts
@@ -10,8 +10,8 @@ export interface CollectionEntryState {
 
 export interface CardCollectionUpdate {
   cardId: string;
-  setId?: string;
-  setGroupId?: string;
+  setId: string;
+  setGroupId: string;
   count: number;
   finish: CardFinish;
   special: CardSpecial;
