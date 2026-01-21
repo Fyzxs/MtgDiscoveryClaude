9f0f3a7d5e9864f8b22103637e71b4cb78052869
Fyzxs <fyzxs@users.noreply.github.com>
2026-01-20 20:30:59 -0800

    fix: Update query definitions to use 'partition' instead of 'user_id' for consistency

--- src/Lib.Adapter.Scryfall.Cosmos/Apis/Operators/Inquisitions/UserCardItemsByArtistQueryDefinition.cs
+++ src/Lib.Adapter.Scryfall.Cosmos/Apis/Operators/Inquisitions/UserCardItemsByArtistQueryDefinition.cs
@@ -5,5 +5,5 @@ namespace Lib.Adapter.Scryfall.Cosmos.Apis.Operators.Inquisitions;
 
 public sealed class UserCardItemsByArtistQueryDefinition : InquiryDefinition
 {
-    public override QueryDefinition AsSystemType() => new("SELECT * FROM c WHERE c.user_id = @userId AND ARRAY_CONTAINS(c.artist_ids, @artistId)");
+    public override QueryDefinition AsSystemType() => new("SELECT * FROM c WHERE c.partition = @userId AND ARRAY_CONTAINS(c.artist_ids, @artistId)");
 }
